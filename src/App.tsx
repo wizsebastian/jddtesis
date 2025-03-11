bc5b7a3d15362b6648bb94cce74d95e0d6995fa8
Luis Vasquez <wizsebastian@gmail.com>
2025-03-10 23:30:46 -0400

    refactor build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
